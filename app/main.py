db5a7fbb8feb76e953eb0b589e3b8d2528650b76
maxwell <cubanmaxramos@gmail.com>
2025-05-11 19:14:35 -0100

    adding demo route

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.health.controller import router as health_routes
+from app.routes.demo.controller import router as demo_routes
 
 origins = [
     "http://localhost",
@@ -18,4 +19,5 @@ app.add_middleware(
 )
 
 
+app.include_router(demo_routes, prefix="/v1")
 app.include_router(health_routes, prefix="/v1")
